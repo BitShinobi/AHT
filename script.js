9c3e4f77234f486d76ab824f473250e2731a9ae1
BitShinobi <157425149+BitShinobi@users.noreply.github.com>
2024-01-23 03:42:52 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+$(document).ready(function () {
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+    // toggle mobile menu
+    $('[data-toggle="toggle-nav"]').on('click', function () {
+        $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
+        return false;
+    });
 
+    // feather icons
+    feather.replace();
 
-function myFunction2() {
-  var dots = document.getElementById("dots2");
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn2");
+    // smooth scroll
+    var scroll = new SmoothScroll('a[href*="#"]');
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+    // tiny slider
+    $('#slider-1').slick({
+        infinite: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+    });
 
-
-
-function myFunction3() {
-  var dots = document.getElementById("dots3");
-  var moreText = document.getElementById("more3");
-  var btnText = document.getElementById("myBtn3");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+    $('#slider-2').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        customPaging: function (slider, i) {
+            return '<div class="bg-white br-round w-1 h-1 opacity-50 mt-5" id=' + i + '> </div>'
+        },
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, ]
+    });
+});
